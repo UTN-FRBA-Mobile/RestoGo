6b4e725d16b0fb41e77bb190e24b331ff45b2cce
Estefania <estefania.ces@gmail.com>
2017-05-28 02:05:12 -0300

    Ahora se pueden recibir notificaciones de la consola de firebase

---
+++ RestoGo/app/src/main/java/ar/com/utn/restogo/FCMInstanceIdService.java
@@ -0,0 +1,32 @@
+package ar.com.utn.restogo;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import java.util.Date;
+
+import static com.google.android.gms.internal.zzt.TAG;
+
+public class FCMInstanceIdService extends FirebaseInstanceIdService {
+
+    public FCMInstanceIdService() {
+    }
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+        Log.d(TAG, "Creada el " + new Date(FirebaseInstanceId.getInstance().getCreationTime()));
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String refreshedToken) {
+    }
+}
