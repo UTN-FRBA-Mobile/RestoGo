4a8ceb79045734a40e4c4079c030bd7ff7acfb97
Federico Oses <osesfm@gmail.com>
2017-06-28 21:07:49 -0300

    Seleccionar fecha, hora, y cantidad de personas

--- RestoGo/app/src/main/java/ar/com/utn/restogo/ReservarFragment.java
+++ RestoGo/app/src/main/java/ar/com/utn/restogo/ReservarFragment.java
@@ -1,21 +1,44 @@
 package ar.com.utn.restogo;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.Calendar;
+
 import ar.com.utn.restogo.modelo.Restaurante;
+import butterknife.BindView;
 
 public class ReservarFragment extends Fragment {
     public static final String RESTAURANTE_KEY = "RESTAURANTE_KEY";
 
     private FirebaseAuth auth;
     private Restaurante restaurante;
+    private Integer cantidad;
+
+    private static TextView textDia;
+    private ImageButton buttonDia;
+    private static TextView textHora;
+    private ImageButton buttonHora;
+    private ImageButton buttonMinus;
+    private TextView textCantidad;
+    private ImageButton buttonPlus;
+    private Button butttonConfirmar;
 
     public static ReservarFragment newInstance(Restaurante restaurante) {
         ReservarFragment f = new ReservarFragment();
@@ -30,18 +53,115 @@ public class ReservarFragment extends Fragment {
         super.onCreate(savedInstanceState);
         View output = inflater.inflate(R.layout.fragment_reservar, container, false);
         this.restaurante = (Restaurante) getArguments().getSerializable(RESTAURANTE_KEY);
-        auth = FirebaseAuth.getInstance();
-
         return output;
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        textDia = (TextView) getView().findViewById(R.id.dia_Text);
+        buttonDia = (ImageButton) getView().findViewById(R.id.button_dia);
+        textHora = (TextView) getView().findViewById(R.id.horaText);
+        buttonHora = (ImageButton) getView().findViewById(R.id.button_hora);
+        buttonMinus = (ImageButton) getView().findViewById(R.id.button_minus);
+        textCantidad = (TextView) getView().findViewById(R.id.text_value);
+        buttonPlus = (ImageButton) getView().findViewById(R.id.button_plus);
+        butttonConfirmar = (Button) getView().findViewById(R.id.btnConfirmarReserva);
+
+        auth = FirebaseAuth.getInstance();
+        cantidad = 0;
+        textCantidad.setText(cantidad.toString());
+
+        buttonDia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment newFragment = new DatePickerFragment();
+                newFragment.show(getActivity().getSupportFragmentManager(), "datePicker");
+            }
+        });
+
+        buttonHora.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment newFragment = new TimePickerFragment();
+                newFragment.show(getActivity().getSupportFragmentManager(), "timePicker");
+            }
+        });
+
+        buttonMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (cantidad>0) {
+                    cantidad--;
+                }
+                textCantidad.setText(cantidad.toString());
+            }
+        });
+
+        buttonPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cantidad++;
+                textCantidad.setText(cantidad.toString());
+            }
+        });
+
+        butttonConfirmar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
     @Override
     public void onResume() {
         super.onResume();
+    }
+
+    public static class TimePickerFragment extends DialogFragment
+            implements TimePickerDialog.OnTimeSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current time as the default values for the picker
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+
+            // Create a new instance of TimePickerDialog and return it
+            return new TimePickerDialog(getActivity(), this, hour, minute,DateFormat.is24HourFormat(getActivity()));
+        }
+
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            textHora.setText(new StringBuilder().append(pad(hourOfDay)).append(":").append(pad(minute)));
+        }
+    }
+
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            textDia.setText(new StringBuilder().append(pad(day)).append("/").append(pad(month)).append("/").append(pad(year)));
+        }
+    }
+
+    private static String pad(int c) {
+        if (c >= 10)
+            return String.valueOf(c);
+        else
+            return "0" + String.valueOf(c);
     }
 }
